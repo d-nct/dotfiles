70cb695ac67bec9fab796df7932f1595d0858e22
Daniel Nocito <daniel.nocito@aeroespacial.ufrj.br>
2025-06-05 15:08:33 -0300

    Ajeita a identação

--- update_dotfiles.sh
+++ update_dotfiles.sh
@@ -9,35 +9,35 @@ mkdir -p "$BACKUP_DIR"
 # Faz o backup dos arquivos individuais
 echo "Iniciando backup dos dotfiles..."
 for FILE in "${DOTFILES[@]}"; do
-    if [ -f "$FILE" ]; then
-        echo -n "Salvando $FILE... " 
-        rsync -a "$FILE" "$BACKUP_DIR" # -a mantém as permissões
-        echo "Sucesso!"
-    else
-        echo "Não encontrado: $FILE..."
-    fi
+  if [ -f "$FILE" ]; then
+    echo -n "Salvando $FILE... " 
+    rsync -a "$FILE" "$BACKUP_DIR" # -a mantém as permissões
+    echo "Sucesso!"
+  else
+    echo "Não encontrado: $FILE..."
+  fi
 done
 
 # Faz o backup dos diretórios
 echo -e "\nIniciando backup dos diretórios..."
 for DIR in "${DIRECTORIES[@]}"; do
-    if [ -d "$DIR" ]; then
-        DIR_NAME=$(basename "$DIR")
-	ARCHIVE="$BACKUP_DIR/${DIR_NAME}.tar.gz"
+  if [ -d "$DIR" ]; then
+    DIR_NAME=$(basename "$DIR")
+    ARCHIVE="$BACKUP_DIR/${DIR_NAME}.tar.gz"
 
-        echo -n "Salvando o diretório $DIR como tarball... "
-	tar -czf "$ARCHIVE" -C "$(dirname "$DIR")" "$DIR_NAME"
-        echo "Sucesso!"
-    else
-        echo "Diretório não encontrado: $DIR..."
-    fi
+    echo -n "Salvando o diretório $DIR como tarball... "
+    tar -czf "$ARCHIVE" -C "$(dirname "$DIR")" "$DIR_NAME"
+    echo "Sucesso!"
+  else
+    echo "Diretório não encontrado: $DIR..."
+  fi
 done
 
 # Commita as mudanças
 echo -n -e "\nRealizando commit... "
 
 if [ ! -d ".git" ]; then
-    git init
+  git init
 fi
 
 git add .
